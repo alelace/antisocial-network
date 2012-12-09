1cda4b31e70fae7711c36476171374c56fa2a3d7
Dan Fairs <dan@fezconsulting.com>
2012-12-09 21:01:22 +0000

    Finish off the plumbing

--- antisocial/urls.py
+++ antisocial/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import patterns, url
-from .views import ego
+from .views import ego, logout
 
 urlpatterns = patterns(
     '',
     url(r'^ego/$', ego, name='antisocial_ego'),
+    url(r'^logout/$', logout, name='antisocial_logout')
 )
--- antisocial/views.py
+++ antisocial/views.py
@@ -1,3 +1,6 @@
+from django.contrib.auth import logout as auth_logout
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
 from django.views.generic.base import TemplateView
 from lazysignup.decorators import allow_lazy_user
 
@@ -5,3 +8,8 @@ from lazysignup.decorators import allow_lazy_user
 class Ego(TemplateView):
     template_name = 'antisocial/ego.html'
 ego = allow_lazy_user(Ego.as_view())
+
+
+def logout(request):
+    auth_logout(request)
+    return HttpResponseRedirect(reverse('asn_home'))
